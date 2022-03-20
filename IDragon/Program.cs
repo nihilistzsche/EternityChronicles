eceadc7abbbff437a96a75853b17a812b09f8535
Nihilistzsche <nihilistzsche@gmail.com>
2022-03-20 01:56:01 -0700

    Add command line parsing to IDragon to make it look more like a real binary.

--- IDragon/Program.cs
+++ IDragon/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using CommandLine;
 using IronDragon;
 using IronDragon.Runtime;
 using static System.IO.Path;
@@ -10,38 +12,50 @@ namespace IDragon
     {
         private static readonly DragonScope ContextRootScope = new DragonScope();
 
+        private static void ParseOptions(Options o)
+        {
+            if (o.Version) Console.WriteLine("IDragon 0.9: Interactive IronDragon interpreter.");
+
+            var runtime = Dragon.CreateRuntime();
+            var engine = runtime.GetEngine("IronDragon");
+
+            foreach (var arg in o.FileNames)
+            {
+                if (arg.Contains(DirectorySeparatorChar))
+                {
+                    var parts = arg.Split(DirectorySeparatorChar);
+                    Dragon.SetCurrentDirectory(string.Join(DirectorySeparatorChar.ToString(),
+                                                           parts.Take(parts.Length - 1)));
+                }
+                else
+                {
+                    Dragon.SetCurrentDirectory(null);
+                }
+
+                var source = engine.CreateScriptSourceFromFile(arg);
+                var scope = engine.CreateScope();
+                ContextRootScope.MergeIntoScope(scope);
+                source.Execute(scope);
+                ContextRootScope.MergeWithScope(scope);
+            }
+        }
+
         public static void Main(string[] args)
         {
             var runtime = Dragon.CreateRuntime();
             var engine = runtime.GetEngine("IronDragon");
-
-            if (args.Any())
-            {
-                foreach (var arg in args)
-                {
-                    if (arg.Contains(DirectorySeparatorChar))
-                    {
-                        var parts = arg.Split(DirectorySeparatorChar);
-                        Dragon.SetCurrentDirectory(string.Join(DirectorySeparatorChar.ToString(),
-                                                               parts.Take(parts.Length - 1)));
-                    }
-                    else
-                    {
-                        Dragon.SetCurrentDirectory(null);
-                    }
-
-                    var source = engine.CreateScriptSourceFromFile(arg);
-                    source.Execute();
-
-                    return;
-                }
-            }
 
             if (Console.IsInputRedirected)
             {
                 var stdin = Console.In.ReadToEnd();
                 var source = engine.CreateScriptSourceFromString(stdin);
                 source.Execute();
+                return;
+            }
+
+            if (args.Any())
+            {
+                Parser.Default.ParseArguments<Options>(args).WithParsed(ParseOptions);
                 return;
             }
 
@@ -59,6 +73,16 @@ namespace IDragon
 
                 ContextRootScope.MergeWithScope(scope);
             }
+        }
+
+        private sealed class Options
+        {
+            [Option('v', "version", Required = false, HelpText = "Shows the version info and exits.")]
+            public bool Version { get; set; }
+
+            [Value(0, Required = false, MetaName = "Input files",
+                   HelpText = "[file1.dragon] [file2.dragon] ... [fileN.dragon]")]
+            public IEnumerable<string> FileNames { get; set; }
         }
     }
 }
