df6cf22b7933e92e802e1f6476bdc3aafde85e07
Nihilistzsche <nihilistzsche@gmail.com>
2022-03-26 08:27:12 -0700

    Fix the update script.

--- hooks/upd_compiler_version.sh
+++ hooks/upd_compiler_version.sh
@@ -1,2 +1,2 @@
-version=$(xidel $1/ECMSBuildTasks.csproj -s -e '//PackageReference[@Include="Microsoft.Net.Compilers.Toolset"]/Version')
-sed -i -e "s/CompilerVersion =&gt; .+;/CompilerVersion = \"$version\";/" $1/ECXModuleDLLTask.cs
+version=$(xidel ECMSBuildTasks.csproj -s -e '//PackageReference[@Include="Microsoft.Net.Compilers.Toolset"]/Version')
+sed -i -e "s/CompilerVersion => ".*";/CompilerVersion => \"$version\";/" ECXModuleDLLTask.cs
